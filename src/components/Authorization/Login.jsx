17ef84884821c49aac07b08077b5b1e292dabbe1
nnesteruk <nikita.nesteruck57@gmail.com>
2023-02-22 06:44:32 +0300

    add env, scss

---
+++ src/components/Authorization/Login.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import icon from '../../assets/img/logo192.png';
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const onFinish = (values) => {
+    axios
+      .post(`${process.env.REACT_APP_BASEURL}/auth/login`, values)
+      .then(({ data }) => {
+        localStorage.setItem('token', data.token);
+        navigate('/todo');
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+
+  return (
+    <div className="login">
+      <img className="login__icon" src={icon} alt="Icon" />
+      <h1 className="login__title">Вход</h1>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}>
+        <Form.Item
+          className="login-form__input"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email!',
+            },
+          ]}>
+          <Input
+            className="login-form__input-email"
+            prefix={<UserOutlined />}
+            placeholder="Email"
+          />
+        </Form.Item>
+
+        <Form.Item
+          className="login-form__input"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}>
+          <Input.Password
+            className="login-form__input-password"
+            prefix={<LockOutlined />}
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <Form.Item className="login-form__buttons">
+          <Button className="login-form__buttons-enter" type="primary" htmlType="submit">
+            Войти
+          </Button>
+          <p className="login-form__buttons-registration">
+            <Link to="/registration">Зарегистрироваться</Link>
+          </p>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
---
+++ src/components/Authorization/Registration.jsx
@@ -0,0 +1,157 @@
+import { Button, Form, Input, InputNumber, Select } from 'antd';
+import axios from 'axios';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+const { Option } = Select;
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+
+export const Registration = () => {
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    axios
+      .post(`${process.env.REACT_APP_BASEURL}/users/register`, values)
+      .then((response) => response)
+      .catch((error) => error);
+    alert('Registration is succsessfuly');
+    navigate('/Todo-List/');
+  };
+
+  return (
+    <div className="registration">
+      <h1 className="registration__title">Регистрация</h1>
+      <Form
+        {...formItemLayout}
+        className="registration-form"
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        style={{
+          maxWidth: 600,
+        }}
+        scrollToFirstError>
+        <Form.Item
+          className="registration-form__input"
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="registration-form__input"
+          name="password"
+          label="Password"
+          tooltip="'Пароль должен быть длиной не менее 8 символов, из них минимум 1 заглавная буква, 1 прописная, 1 число и 1 символ'"
+          rules={[
+            {
+              required: true,
+              message:
+                'Пароль должен быть длиной не менее 8 символов, из них минимум 1 заглавная буква, 1 прописная, 1 число и 1 символ',
+              pattern: /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g,
+            },
+          ]}
+          hasFeedback>
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          className="registration-form__input"
+          name="name"
+          label="Name"
+          tooltip="What is your name?"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your name!',
+              whitespace: true,
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="registration-form__input"
+          name="username"
+          label="Username"
+          tooltip="What do you want others to call you?"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+              whitespace: true,
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          className="registration-form__input"
+          name="age"
+          label="Age"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your age!',
+            },
+          ]}>
+          <InputNumber />
+        </Form.Item>
+
+        <Form.Item
+          className="registration-form__input"
+          name="isMan"
+          label="isMan"
+          rules={[
+            {
+              required: true,
+              message: 'Please select isMan!',
+            },
+          ]}>
+          <Select placeholder="select your isMan">
+            <Option value={true}>Male</Option>
+            <Option value={false}>Female</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item className="registration-form__button">
+          <Button className="registration-form__button-register" type="primary" htmlType="submit">
+            Register
+          </Button>
+          <p>
+            Уже есть аккаунт?{' '}
+            <NavLink to="/Todo-List" className={({ isActive }) => (isActive ? '' : 'active-link')}>
+              Войти
+            </NavLink>
+          </p>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
